26fc1c3052906730a0bfa62da613223c508785ae
Gabriel Chaves <gabrieloliveirachaves754@gmail.com>
2021-12-21 17:13:24 +0000

    feat: css variables

---
+++ src/styles/theme.js
@@ -0,0 +1,15 @@
+export const theme = {
+  fonts: {
+    roboto: 'Roboto',
+    poppins: 'Poppins',
+  },
+  colors: {
+    black: '#080808',
+  },
+  sizes: {
+    xlarge: '4.5rem',
+    large: '1.8rem',
+    medium: '1.2rem',
+  },
+  spaces: {},
+};
